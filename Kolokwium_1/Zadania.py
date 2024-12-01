269d305a4803ca674b3cb78e3567feea3bd083e4
cissuperior <101932740+cissuperior@users.noreply.github.com>
2024-12-01 17:23:48 +0100

    Dodanie zadań do kolokwium

---
+++ Kolokwium_1/Zadania.py
@@ -0,0 +1,235 @@
+# Lekcja 05.12.24
+# Zadania powtórzeniowe przed kolokwium
+
+# 1. Zmienne
+# Zadanie 1.
+# Zadeklaruj dwie zmienne 'a' = 5 i 'b' = 7. Oblicz ich sumę, przypisz wynik do zmiennej 'c' i wypisz wynik.
+
+
+# Zadanie 2.
+# Ustaw wartości zmiennych 'długosc' = 10 i 'szerokosc' = 4. Oblicz pole prostokąta i wypisz wynik.
+
+
+# Zadanie 3.
+# Utwórz zmienne x = 3 i y = 8. Zamień ich wartości miejscami i wypisz nowe wartości.
+
+
+# Zadanie 4.
+# Przypisz do zmiennych 'imie' = "Anna" i 'nazwisko' = "Kowalska". Wypisz pełne imię i nazwisko w jednej linii.
+
+
+# Zadanie 5.
+# Przypisz do zmiennej 'temperatura' wartość 25. Wypisz tekst: "Dziś temperatura wynosi 25 stopni Celsjusza",
+# używając zmiennej.
+
+
+# 2. Operatory
+# Zadanie 1.
+# Utwórz zmienne a = 10 i b = 20. Sprawdź, czy a jest mniejsze od b, i wypisz wynik.
+
+
+# Zadanie 2.
+# Zadeklaruj zmienne x = 15 i y = 4. Wypisz wynik dzielenia oraz resztę z dzielenia tych liczb.
+
+
+# Zadanie 3.
+# Utwórz zmienną n = 3. Oblicz wartość n podniesioną do potęgi 4 i wypisz wynik.
+
+
+# Zadanie 4.
+# Przypisz zmiennym a = 5 i b = 5. Sprawdź, czy są sobie równe, i wypisz odpowiedź w formie True lub False.
+
+
+# Zadanie 5.
+# Ustaw zmienne tekst1 = "Cześć" i tekst2 = "świat!". Połącz te dwa teksty w jeden i wypisz wynik.
+
+
+# Zadanie 6.
+# Zadeklaruj zmienne a = 7 i b = 6. Oblicz iloczyn tych liczb i wyświetl wynik.
+
+
+# Zadanie 7.
+# Utwórz zmienne x = True i y = False. Sprawdź wynik operacji x and y oraz x or y i wypisz odpowiedzi.
+
+
+# 3. Instrukcje Sterowania przepływem
+# Zadanie 1.
+# Napisz program, który sprawdzi, czy liczba zapisana w zmiennej liczba = 8 jest parzysta.
+# Wypisz odpowiedni komunikat.
+
+
+# Zadanie 2.
+# Utwórz zmienne a = 12 i b = 18. Sprawdź, która liczba jest większa, i wypisz ją.
+
+
+# Zadanie 3.
+# Przypisz do zmiennej punkty = 85. Jeśli punkty są większe lub równe 90, wypisz "Ocena: 5".
+# Jeśli są większe lub równe 75, wypisz "Ocena: 4". W przeciwnym wypadku wypisz "Ocena: 3".
+
+
+# Zadanie 4.
+# Ustaw zmienną liczba = -5. Sprawdź, czy liczba jest dodatnia, ujemna,
+# czy równa zero, i wypisz odpowiedni komunikat.
+
+
+# Zadanie 5.
+# Zadeklaruj zmienne login = "admin" i haslo = "1234". Napisz program,
+# który sprawdzi, czy oba te dane są poprawne (wpisane przez użytkownika)
+# i wypisze odpowiedni komunikat ("Zalogowano" lub "Błędne dane").
+
+
+# 4. Pętle
+# Zadanie 1.
+# Napisz program, który wypisze liczby od 1 do 10.
+
+
+# Zadanie 2.
+# Użyj pętli for, aby wypisać kwadraty liczb od 1 do 5.
+
+
+# Zadanie 3.
+# Dla danej listy fruits = ["jabłko", "banan", "czereśnia"] wypisz każdy owoc w osobnej linii.
+fruits = ["jabłko", "banan", "czereśnia"]
+
+
+# Zadanie 4.
+# Dla tej samej listy fruits wypisz każdy owoc ale w jednej linii. Słowa mają być oddzielone spacją.
+
+
+# Zadanie 5.
+# Za pomocą pętli for oblicz sumę liczb od 1 do 20 i wypisz wynik.
+
+
+# Zadanie 6.
+# Napisz program, który dla słowa Python wypisze każdą literę w osobnej linii.
+
+
+# 5. Input/Output
+# Zadanie 1.
+# Poproś użytkownika o podanie dwóch liczb. Oblicz ich sumę i wypisz wynik.
+
+
+# Zadanie 2.
+# Zapytaj użytkownika o jego ulubiony kolor, a następnie wypisz komunikat w stylu: "Twój ulubiony kolor to [kolor]".
+
+
+# Zadanie 3.
+# Poproś użytkownika o podanie jego wieku. Oblicz, ile będzie miał lat za 10 lat, i wypisz odpowiedź.
+
+
+# 6. Losowanie Liczb
+
+
+# 7. Listy
+# Zadanie 1.
+# Utwórz listę fruits zawierającą 3 owoce: "jabłko", "banan", "gruszka". Wypisz całą listę.
+
+
+# Zadanie 2.
+# Do listy fruits = ["jabłko", "banan"] dodaj element "czereśnia" i wypisz nową listę.
+
+
+# Zadanie 3.
+# Z listy fruits = ["jabłko", "banan", "gruszka"] wypisz pierwszy owoc.
+
+
+# Zadanie 4.
+# W liście fruits = ["jabłko", "banan", "gruszka"] zamień "banan" na "pomarańcza" i wypisz zmienioną listę.
+
+
+# Zadanie 5.
+# Z listy fruits = ["jabłko", "banan", "gruszka"] usuń element "banan" i wypisz wynikową listę.
+
+
+# Zadanie 6.
+# Sprawdź, czy "jabłko" znajduje się w liście fruits = ["jabłko", "banan", "gruszka"]. Wypisz True lub False.
+
+
+# Zadanie 7.
+# Z listy numbers = [1, 2, 3, 4, 5, 6] wypisz pierwsze trzy elementy.
+
+
+# Zadanie 8.
+# Oblicz sumę elementów listy numbers = [3, 5, 7, 2] i wypisz wynik.
+
+
+# Zadanie 9.
+# Za pomocą pętli enhanced utwórz listę z kolejnymi kwadratami liczb od 1 do 9. Czyli lista ma zawierać:
+# 1, 4, 9, ..., 81
+
+
+# 8. Tuple
+# Zadanie 1.
+# Utwórz krotkę person = ("Jan", 25, "programista") i wypisz ją.
+
+
+# Zadanie 2.
+# Z krotki person = ("Jan", 25, "programista") wypisz drugi element (wiek).
+
+
+# Zadanie 3.
+# Krotki są niezmienne, ale możesz spróbować zmienić element krotki
+# person = ("Jan", 25, "programista"). Spróbuj to zrobić i sprawdź, co się stanie (wypisz komunikat o błędzie).
+
+
+# Zadanie 4.
+# Sprawdź, czy w krotce fruits = ("jabłko", "banan", "gruszka") znajduje się "banan". Wypisz wynik jako True lub False.
+
+
+# Zadanie 5.
+# Utwórz dwie krotki: tuple1 = (1, 2) i tuple2 = (3, 4). Połącz je w jedną krotkę i wypisz wynik.
+
+
+# Zadanie 6.
+# Z krotki fruits = ("jabłko", "banan", "gruszka") oblicz jej długość i wypisz wynik.
+
+
+# 9. Zbiory
+
+
+# 10. Mapy
+
+
+# 11. Funkcje
+# Zadanie 1.
+# Napisz funkcję powitanie(), która wypisuje tekst "Witaj w Pythonie!". Wywołaj ją.
+
+
+# Zadanie 2.
+# Napisz funkcję powitanie(imie), która przyjmuje jedno imię jako argument
+# i wypisuje tekst: "Witaj, [imie]!". Wywołaj ją, przekazując swoje imię.
+
+
+# Zadanie 3.
+# Napisz funkcję dodaj(a, b), która przyjmuje dwie liczby, dodaje je do
+# siebie i zwraca wynik. Wywołaj ją z wartościami 3 i 5, a wynik wypisz.
+
+
+# Zadanie 4.
+# Napisz funkcję opis(imię, wiek=18), która wypisuje tekst: "[imię] ma [wiek] lat".
+# Jeśli wiek nie zostanie podany, domyślnie ma wartość 18.
+# Wywołaj ją raz z wiekiem i raz bez niego.
+
+
+# Zadanie 5.
+# Napisz funkcję mnoz(a, b), która przyjmuje dwie liczby i zwraca ich iloczyn.
+# Wywołaj ją z dwiema liczbami i wypisz wynik.
+
+
+# Zadanie 6.
+# Napisz funkcję drukuj_pozdrowienie(), która nie przyjmuje żadnych argumentów,
+# ale wypisuje "Pozdrawiam!". Wywołaj ją.
+
+
+# Zadanie 7.
+# Napisz funkcję pole_prostokata(dlugosc, szerokosc), która przyjmuje długość i
+# szerokość prostokąta, oblicza pole i zwraca wynik. Wywołaj ją, przekazując wymiary prostokąta.
+
+
+# Zadanie 8.
+# Napisz funkcję czy_parzysta(liczba), która sprawdza, czy podana liczba jest parzysta.
+# Funkcja powinna zwracać True lub False. Wywołaj ją z różnymi liczbami.
+
+
+# 12. Napisy
+
